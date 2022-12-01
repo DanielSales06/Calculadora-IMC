da9de7114b188d72d6094d6919f5317c7a0302af
Daniel Sales <116517871+DanielSales06@users.noreply.github.com>
2022-11-30 21:31:38 -0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+// Imports
+import { Modal } from "./modal.js"
+import { AlertError } from "./alert-error.js"
+import { calculateIMC, noNumber } from "./utils.js"
+
+// Variáveis - Variables
+const form = document.querySelector("form")
+const inputWeight = document.querySelector("#weight")
+const inputHeight = document.querySelector("#height")
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  const weight = inputWeight.value
+  const height = inputHeight.value
+  const weightOrHeightIsNotANumber = noNumber(weight) || noNumber (height)
+
+  if (weightOrHeightIsNotANumber) {
+    AlertError.open()
+    return;
+  }
+
+  AlertError.close()
+
+  const result = calculateIMC(weight, height)
+  displayResultMessage(result)
+}
+
+function displayResultMessage(result) {
+  const message = `Seu IMC é de ${result}`
+
+  Modal.message.innerText = message
+  Modal.open()
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
